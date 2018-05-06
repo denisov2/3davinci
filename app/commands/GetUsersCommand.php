6e2a834494b6c4c24d07e1b0adbafe9ddddffcea
denisov <denisov.dmitriy@gmail.com>
2018-05-06 19:59:09 +0300

    Initial commit

---
+++ app/commands/GetUsersCommand.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: denisov
+ * Date: 06.05.2018
+ * Time: 19:03
+ */
+
+namespace app;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GetUsersCommand extends Command
+{
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // ...
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: denisov
+ * Date: 06.05.2018
+ * Time: 18:54
+ */
